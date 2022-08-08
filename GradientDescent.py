249766f13df592a64a96f9da96da5ee9bf5f9ff4
JonathanCecil01 <jonathansesuraj@gmail.com>
2022-08-08 09:19:34 +0530

    Gradient Descent for any Equation in one variable

--- GradientDescent.py
+++ GradientDescent.py
@@ -6,4 +6,21 @@ This is a temporary script file.
 """
 
 #import numpy as np
-print("Hell0")
+import numpy as np
+import sympy as sp 
+
+x, y = sp.symbols("x y")
+
+expr = (x+5)**2
+dexpr = sp.diff(expr, x)
+lr = 0.01
+var =3
+dfx = dexpr.evalf(subs = {x : 3})
+diff = var - lr*dfx
+while(diff>0.0000001):
+    var = diff
+    print(diff)
+    diff = diff = var - lr*dfx
+
+print(var)
+print(diff)
